cb3fe9966ecdc3acc12ccf18b9221b2a24b26597
atwanzireT <tatwanzire@gmail.com>
2023-03-09 22:38:51 +0000

    workingon the UI

--- app/src/main/java/com/example/harvest/CreatePostFragment.java
+++ app/src/main/java/com/example/harvest/CreatePostFragment.java
@@ -2,11 +2,21 @@ package com.example.harvest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,4 +71,5 @@ public class CreatePostFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_post, container, false);
     }
+
 }
