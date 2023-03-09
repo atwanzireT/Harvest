7119315055d45b07b25f039e7a87bc8ae5a393f5
atwanzireT <tatwanzire@gmail.com>
2023-03-09 11:59:09 +0000

    first commit

---
+++ app/src/main/java/com/example/harvest/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.harvest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        HomeFragment homeFragment = new HomeFragment();
+        NewsFragment newsFragment = new NewsFragment();
+        SettingFragment settingFragment = new SettingFragment();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new HomeFragment() ).commit();
+                return true;
+
+            case R.id.news:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new NewsFragment()).commit();
+                return true;
+
+            case R.id.settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new SettingFragment()).commit();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
